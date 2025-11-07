6085157c6649ad29703d7cf5132d32cb7a37b091
Alex Escriva Fuset <alfues62@gmail.com>
2025-11-07 09:12:20 +0100

    Volumenes y MarcasBien

---
+++ Assets/Scripts/volumeManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI; // Asegúrate de tener esta línea para el Slider
+
+public class volumeManager : MonoBehaviour
+{
+    [Header("UI")]
+    public Slider volumeSlider;
+
+    // Esta es la clave donde guardaremos el volumen
+    private const string VOLUME_KEY = "MasterVolume";
+
+    void Start()
+    {
+        // 1. Cargar el volumen guardado. Si no hay nada, usa 1.0 (máximo)
+        float savedVolume = PlayerPrefs.GetFloat(VOLUME_KEY, 1.0f);
+
+        // 2. Aplicar ese volumen a todo el juego
+        SetMasterVolume(savedVolume);
+
+        // 3. Poner el slider en la posición correcta
+        if (volumeSlider != null)
+        {
+            volumeSlider.value = savedVolume;
+
+            // 4. Añadir el "listener" para que la función se llame cuando muevas el slider
+            volumeSlider.onValueChanged.AddListener(OnVolumeSliderChanged);
+        }
+    }
+
+    // Esta función se llama CADA VEZ que el usuario mueve el slider
+    public void OnVolumeSliderChanged(float value)
+    {
+        SetMasterVolume(value);
+    }
+
+    // Función central para aplicar y guardar el volumen
+    private void SetMasterVolume(float value)
+    {
+        // 1. Aplicar el valor al "oído" principal del juego
+        // Esto afecta a TODOS los AudioSources
+        AudioListener.volume = value;
+
+        // 2. Guardar el valor en el dispositivo
+        PlayerPrefs.SetFloat(VOLUME_KEY, value);
+        PlayerPrefs.Save(); // Forzar el guardado
+
+        Debug.Log($"Volumen maestro guardado y aplicado: {value}");
+    }
+}
---
+++ Assets/tareas/JumbotronConVideo/jumbotronTexts.cs
@@ -0,0 +1,119 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class jumbotronTexts : MonoBehaviour
+{
+
+    public Text[] roundTexts;
+    public Text[] timeTexts;
+    public Text[] stateTexts;
+
+    private bool timeTicking = false;
+    private float actualTiming = 0f;
+
+    public ParticleSystem confettiParticles;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ShowTexts(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeTicking){
+            UpdateTimeJumbotron();
+        }
+    }
+
+    public void Victory() {
+        //Para cuando el jugador gana
+        //ocultamos los demás textos y solo mostramos el victoria con fondo verde (tambien hay confeti)
+        ShowTexts(2);
+        confettiParticles.Play();
+    }
+
+    public void Loose() {
+        // Para cuando el jugador pierde
+        //ocultamos los demás textos y solo mostramos el derrota con fondo rojo
+        ShowTexts(3);
+    }
+
+    public void ShowTexts(int textType) {
+        //textTyoe: 1=tiempo, 2=victoria, 3=derrota
+        switch (textType){
+            case 1:
+                //Mostramos el tiempo
+                foreach(var textoTiempo in timeTexts) {
+                    textoTiempo.enabled = true;
+                }
+                foreach(var textoEstado in stateTexts) {
+                    textoEstado.enabled = false;
+                }
+                break;
+
+            case 2:
+                //Ocultamos tiempo y mostramos el texto de victoria
+                foreach(var textoTiempo in timeTexts) {
+                    textoTiempo.enabled = false;
+                }
+                foreach(var textoEstado in stateTexts) {
+                    textoEstado.enabled = true;
+                    textoEstado.text = "VICTORIA";
+                }
+                break;
+
+            case 3:
+                //Ocultamos tiempo y mostramos el texto de derrota
+                foreach(var textoTiempo in timeTexts) {
+                    textoTiempo.enabled = false;
+                }
+                foreach(var textoEstado in stateTexts) {
+                    textoEstado.enabled = true;
+                    textoEstado.text = "DERROTA";
+                }
+                break;
+        }
+    }
+
+    private string parseTimeToText(float timeOriginal) {
+        if (timeOriginal < 0){
+            return "00:00";
+        }
+        // Separmos segundos y milésimas
+        int segundos = Mathf.FloorToInt(timeOriginal);
+        int milesimas = Mathf.FloorToInt((timeOriginal - segundos) * 100);
+
+        return string.Format("{0:00}:{1:00}", segundos, milesimas);
+    }
+
+    public void SetTimeJumbotron(float time) {
+        actualTiming = time;
+        foreach(var timeTxt in timeTexts) {
+            timeTxt.text = parseTimeToText(time);
+        }
+    }
+
+    public void StartTimeJumbotron() {
+        timeTicking = true;
+    }
+    public void StopTimeJumbotron() {
+        timeTicking = false;
+    }
+
+    public void UpdateTimeJumbotron() {
+        actualTiming -= Time.deltaTime;
+        string text = parseTimeToText(actualTiming);
+        foreach(var timeTxt in timeTexts) {
+            timeTxt.text=text;
+        }
+    }
+
+    public void SetRoundNumJumbotron(int roundNum) {
+        foreach(var roundTxt in roundTexts) {
+            roundTxt.text ="RONDA "+ roundNum.ToString() + "/3";
+        }
+    }
+}
